af12e3f023b5701792cec7e95fbf1409aec018c0
Vadim Kovalenko <vadblaxmith@gmail.com>
2017-05-22 15:49:38 +0200

    3d commit

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,3 +1,8 @@
-// $( document ).ready(function() {
-
-// });
+$( document ).ready(function() {
+	$(".calendar-table td").click(function() {
+		  // remove classes from all
+		  $(this).closest(".calendar-table").find("td").removeClass("active");
+		  // add class to the one we clicked
+		  $(this).addClass("active");
+		});
+});
